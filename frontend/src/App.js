6c4406d19f5320a2ea04fb3906020220ae639daf
Samuel Obunge <samuelobunge@gmail.com>
2024-09-10 14:08:47 +0300

    adding a not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Main from './pages/Main';
 import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import Settings from './pages/Settings';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -11,11 +14,20 @@ function App() {
     <AuthProvider>
       <Router>
         <Routes>
+          {/* Unprotected Routes */}
           <Route path="/" element={<Main />} />
           <Route path='/login' element={<Login />} />
+
+          {/* Protected Routes */}
           <Route element={<PrivateRoute />}>
+            {/* All routes inside this block are protected */}
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Settings />} />
           </Route>
+
+          {/* Fallback Route for 404 */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
     </AuthProvider>
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -25,23 +25,27 @@ function Login() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                required
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                required
-            />
-            <button type="submit">Login</button>
-        </form>
+
+        <div>
+            <h1> Login </h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
     );
 }
 
